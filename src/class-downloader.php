7428bcfeb833663abdbecaed3c592010f5825263
ronchambers <116242607+ronchambers@users.noreply.github.com>
2026-01-06 14:38:01 -0800

    Refector Get All Urls with in-valid: rollback condensed if block

--- src/class-downloader.php
+++ src/class-downloader.php
@@ -424,14 +424,18 @@ class Downloader {
 				// Filter by host.
 				if ( $only_scan_hosts ) {
 
-					// If '/' urls are allowed and it's actually a root relative url.
-					$is_allowed_root_relative = in_array( '/', $only_scan_hosts, true ) && $this->is_url_root_relative( $url );
-
-					// If url matches an allowed host.
-					$is_allowed_host = $this->does_uri_match_host( $url, $only_scan_hosts );
+					$allow_host = false;
 					
-					// Skip the url if both checks are false.
-					if ( ! $is_allowed_root_relative && ! $is_allowed_host ) {
+					// Allow root-relative ("/something...") if '/' was set in --only-scan-hosts.
+					if ( in_array( '/', $only_scan_hosts, true ) && $this->is_url_root_relative( $url ) ) {
+						$allow_host = true;
+					}
+					elseif ( $this->does_uri_match_host( $url, $only_scan_hosts ) ) {
+						// Hostname match.
+						$allow_host = true;
+					}
+
+					if ( ! $allow_host ) {
 						$this->log( self::LOG_OUTPUTS['CLI_AND_FILE'], LogLevel::DEBUG, sprintf( "✖ skipping, off target host '%s'", $url ), [ 'post_id' => $post_id ] );
 						continue;
 					}
